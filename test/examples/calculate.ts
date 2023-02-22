7811ff8e35a50bbf80f5be2b4d255a35eb894f21
msrocka <michael.srocka@gmail.com>
2023-02-22 15:56:21 +0100

    example works

---
+++ test/examples/calculate.ts
@@ -0,0 +1,59 @@
+import * as o from "../../mod.ts";
+
+async function main() {
+  // we can use a client for the REST or IPC (JSON-RPC) protocol
+  // both clients implement the same interface
+  const protocol: "REST" | "IPC" = "IPC";
+  const client = protocol === "IPC"
+    ? o.IpcClient.on(8080)
+    : o.RestClient.on(8080);
+
+  // select a product system
+  const systems = await client.getDescriptors(o.RefType.ProductSystem);
+  if (systems.length === 0) {
+    console.log("error: no product systems found");
+    return;
+  }
+  const system = systems[0];
+  console.log(`calculate system: ${system.name}`);
+
+  // select an impact assessment method, if available
+  const methods = await client.getDescriptors(o.RefType.ImpactMethod);
+  const method = methods.length >= 0 ? methods[0] : null;
+  if (!method) {
+    console.log("  no LCIA method available");
+  } else {
+    console.log(`  using LCIA method: ${method.name}`);
+  }
+
+  // calculate the system
+  console.log("  ... calculating ...");
+  const setup = o.CalculationSetup.of({
+    target: system,
+    impactMethod: method,
+  });
+  const result = await client.calculate(setup);
+  let state: o.ResultState;
+  while (!(state = await result.getState()).isReady) {
+    if (state.error) {
+      throw Error(`calculation failed: ${state.error}`);
+    }
+    await new Promise((r) => setTimeout(r, 1000));
+    console.log("  ...");
+  }
+  console.log("  done");
+
+  // query the result
+  const impacts = await result.getTotalImpacts();
+  console.log("LCIA Results:");
+  for (const impact of impacts) {
+    const name = impact.impactCategory?.name;
+    const unit = impact.impactCategory?.refUnit;
+    console.log(`  ${name}: ${impact.amount?.toExponential(2)} ${unit}`);
+  }
+
+  // finally, dispose the result
+  result.dispose();
+}
+
+main();
